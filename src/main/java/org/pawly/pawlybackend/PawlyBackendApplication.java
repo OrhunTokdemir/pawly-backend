89d428e51fcd7b29f478e6f08e20002bddc7c16a
OrhunTokdemir <148989093+OrhunTokdemir@users.noreply.github.com>
2026-05-05 13:34:18 +0300

    initialize pawly-backend project with Gradle and Spring Boot setup

---
+++ src/main/java/org/pawly/pawlybackend/PawlyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.pawly.pawlybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawlyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawlyBackendApplication.class, args);
+    }
+
+}
