090e24db668eea44bf3ed0ddf7e76dd2598f9de3
Krekno <yigiterenkarabas@gmail.com>
2026-05-17 14:52:36 +0300

    correct typpo

--- src/main/java/org/pawly/pawlybackend/config/WebSecurityConfig.java
+++ src/main/java/org/pawly/pawlybackend/config/WebSecurityConfig.java
@@ -76,7 +76,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "https://pawlysocial.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "https://pawlysocial.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
